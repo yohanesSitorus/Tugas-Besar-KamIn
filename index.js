e0b5088885f792719014c6efe6d748f3171b651e
otwtajir <galileopurnajaya@gmail.com>
2023-12-05 22:17:51 +0700

    Home + AddVote

--- index.js
+++ index.js
@@ -20,28 +20,76 @@ app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
 
-// import mysql from 'mysql';
-// // MySQL Connection
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'kamin',
-//     "typeCast": function castField(field, useDefaultTypeCasting) {
-//         if ((field.type === "BIT") && (field.length === 1)) {
-//             var bytes = field.buffer();
-//             return (bytes[0]);
-//         }
-//         return (useDefaultTypeCasting());
-//     }
-// });
+import mysql from 'mysql';
+// MySQL Connection
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kamin',
+    "typeCast": function castField(field, useDefaultTypeCasting) {
+        if ((field.type === "BIT") && (field.length === 1)) {
+            var bytes = field.buffer();
+            return (bytes[0]);
+        }
+        return (useDefaultTypeCasting());
+    }
+});
+
+
+app.get('/login', async (req, res) => {
+    res.render('login', { errorMsg: null, success: null });
+})
+
+app.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    const accountQuery =
+        'SELECT `E_mail`, `Password` FROM `account` WHERE `E_mail` = ? AND `Password` = ?';
+    // 'SELECT `Id_Account`,`E_mail`, `Password` FROM `account` WHERE `E_mail` = ? AND `Password` = ?';
+    const accountParams = [email, password];
+
+    pool.query(accountQuery, accountParams, (error, results) => {
+        if (error) {
+            console.log(error);
+        } else if (results.length > 0) {
+            // const user = results[0];
+            // res.cookie('Id_Account', user.Id_Account)
+            // res.cookie('email', user.E_mail);
+            // res.cookie('role', user.);
+
+            // if (user.Account_Role === 'admin') {
+            //     res.redirect('/admin-menu');
+            // }
+            // else if (user.Account_Role === 'dosen') {
+            //     res.redirect('/menu-dosen')
+            // }
+            // else {
+            //     res.redirect('/');
+            // }
+            // res.render('/home');
+            console.log("Successfully validated");
+            res.redirect('/home');
+        } else {
+            res.render('login', {
+                errorMsg: 'Password / email anda salah.',
+                success: false,
+            });
+        }
+    });
+});
 
 app.get('/', async (req, res) => {
     res.redirect('/home');
-    // res.render('layout')
+    
 })
 
 app.get('/home', async (req, res) => {
     res.render('home');
-    // res.render('layout')
+    
+})
+
+app.get('/AddVote', async (req, res) => {
+    res.render('AddVote');
+    
 })
